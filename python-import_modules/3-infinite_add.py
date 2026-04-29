661e1cb5f05c3fce4e1b36f81342e5ba7c030892
IamAMONDI <e.otieno1@alustudent.com>
2026-04-29 16:24:37 +0400

    3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+import sys
+def main():
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
 if __name__ == "__main__":
-    from sys import argv
-    count = len(argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+    main()
