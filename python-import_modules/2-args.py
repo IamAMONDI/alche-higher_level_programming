3d6768188c037d0d19d50568bf0924e9c7474e6f
IamAMONDI <e.otieno1@alustudent.com>
2026-05-05 14:30:25 +0400

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-    count=len(sys.argv) - 1.
-    if count==0:
+
+def main():
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif count==1:
+    elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for i in range(1,count+1):
-        print("{}: {}".format(i,sys.argv[i]))
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+
+1if __name__ == "__main__":
+    main()
