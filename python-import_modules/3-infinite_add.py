f92ce01cba09bdb10b799cbc6975e279d0a1a677
IamAMONDI <e.otieno1@alustudent.com>
2026-05-05 14:36:00 +0400

    3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
-def main():
-    total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
-    print(total)
-
-
 if __name__ == "__main__":
-    main()
+    total = sum(int(arg) for arg in sys.argv[1:])
+    print(total)
