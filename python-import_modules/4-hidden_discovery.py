1002821bebf8834910f5aa8ab5e09657eb22ca44
IamAMONDI <e.otieno1@alustudent.com>
2026-05-05 14:37:42 +0400

    4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    module_names = dir(hidden_4)
+    filtered_names = sorted([name for name in module_names if not name.startswith("__")])
+    for name in filtered_names:
+        print(name)
